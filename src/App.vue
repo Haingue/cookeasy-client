b45300c2e3be9ded08763d6f556610b4270052ff
haingue <fabien.haingue@gmail.com>
2023-04-10 11:26:22 +0200

    FEAT: add first page to test backend

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    header
+  </header>
+  <main>
+    Main
+    <br/>
+    <product-form />
+    <br/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </main>
+  <footer>
+    footer
+  </footer>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ProductForm from './components/ProductForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ProductForm
   }
 }
 </script>
---
+++ src/components/ProductForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit="saveProduct">
+    <input v-model="name" type="text" maxlength="64" />
+    <input v-model="descripton" type="text" maxlength="64" />
+    <input v-model="nutritionalScore" type="number" min=0 max=5 step=0.5 />
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ProductForm',
+  props: {
+    productId: String
+  },
+  data: () => ({
+    name: null,
+    descripton: null,
+    nutritionalScore: null,
+    type: [],
+    category: [],
+    allergens: [],
+    isBusy: false
+  }),
+  methods: {
+    saveProduct: async function () {
+      this.isBusy = true
+      const product = {
+        name: this.name,
+        descripton: this.descripton,
+        nutritionalScore: this.nutritionalScore,
+        type: this.type,
+        category: this.category,
+        allergens: this.allergens
+      }
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      console.debug(`Save product: ${this.name}`, product)
+      this.isBusy = false
+    }
+  }
+}
+</script>
